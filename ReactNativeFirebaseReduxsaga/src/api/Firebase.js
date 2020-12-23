a0737b9559d2f41c2feec1ff54d17bdf0d1bac16
Zenga 1 <>
2020-12-23 15:08:01 +0700

    update sagaUser

--- ReactNativeFirebaseReduxsaga/src/api/Firebase.js
+++ ReactNativeFirebaseReduxsaga/src/api/Firebase.js
@@ -1,32 +1,25 @@
 import auth from '@react-native-firebase/auth';
 
 export const User = {
-    loginWithEmail: (userInfo) => {
-        return auth()
-            .signInWithEmailAndPassword(userInfo.email, userInfo.password)
-            .then(() => {
-                console.log("login succesful!");
-            })
-            .catch(error => {
-                console.log(error)
-            })
+    loginWithEmail: async (userParam) => {
+        const response = await auth().signInWithEmailAndPassword(userParam.userInfo.email, userParam.userInfo.password);
+        return response;
     },
-    signUpWithEmailPassword: () => {
-        return auth()
-            .createUserWithEmailAndPassword('jane.doe@example.com', 'SuperSecretPassword!')
-            .then(() => {
-                console.log('User account created & signed in!');
-            })
-            .catch(error => {
-                if (error.code === 'auth/email-already-in-use') {
-                    console.log('That email address is already in use!');
-                }
+    signUpWithEmailPassword: async () => {
+        try {
+            await auth()
+                .createUserWithEmailAndPassword('jane.doe@example.com', 'SuperSecretPassword!');
+            console.log('User account created & signed in!');
+        } catch (error) {
+            if (error.code === 'auth/email-already-in-use') {
+                console.log('That email address is already in use!');
+            }
 
-                if (error.code === 'auth/invalid-email') {
-                    console.log('That email address is invalid!');
-                }
+            if (error.code === 'auth/invalid-email') {
+                console.log('That email address is invalid!');
+            }
 
-                console.error(error);
-            });
+            console.error(error);
+        }
     }
 }
--- ReactNativeFirebaseReduxsaga/src/redux/user/saga.js
+++ ReactNativeFirebaseReduxsaga/src/redux/user/saga.js
@@ -2,13 +2,14 @@ import { select, call, put, take } from 'redux-saga/effects';
 import { User } from '../../api/Firebase';
 
 import * as ActionTypes from './../actionTypes';
+import * as Firebase from './../../api/Firebase';
 
 export function* loginWithEmail(action) {
     try {
         console.log('userSaga action --', action);
         const state = yield select();
         console.log('UserSaga state-- ', state);
-        const response = yield call(User.loginWithEmail(action.userInfo), action);
+        const response = yield call(Firebase.User.loginWithEmail, action);
         console.log('User Saga Response', JSON.stringify(response));
         yield put({ type: ActionTypes.LOGIN_SUCCESS, response })
     }
